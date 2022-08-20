0b13d298b6d845ddbe294971f16c333c8d5f624d
connor-albright <111525314+connor-albright@users.noreply.github.com>
2022-08-20 14:39:51 -0400

    Updated doc strings for deck.py

--- ThreeCardPoker/model/deck.py
+++ ThreeCardPoker/model/deck.py
@@ -2,7 +2,9 @@ from random import sample
 from model.card import Card
 
 class Deck:
+    """This represents the class for the deck."""
     def __init__(self):
+        """Constructs the class Deck(). Creates a set of all 52 cards with value and suit."""
         self.cards = set()
         for card_value in range(2, 15):
             for card_suit in Card.CardSuit:
@@ -10,6 +12,14 @@ class Deck:
 
 
     def get_n_cards(self, n, face_up: bool):
+        """Gets 'n' cards as a random sample and returns said cards
+        and if they are face up or not
+        Args:
+            n (int): number of cards.
+            face_up (bool): determines if the card is faced up or not
+        Returns:
+            tuple: 'n' amount of cards in a tuple.
+        """
         n_cards = sample(self.cards, n)
         for card in n_cards:
             self.cards.remove(card)
@@ -18,8 +28,14 @@ class Deck:
         return tuple(n_cards)
     
     def return_used_cards(self, used_cards):
+        """Returns cards back to the deck.
+        Args:
+            used_cards (tuple): said cards to be returned to the deck.
+        """
         for card in used_cards:
             self.cards.add(card)
 
     def __repr__(self) -> str:
+        """String representation of the Deck. Returns amount of cards
+        left in the deck."""
         return f'The Current Deck Has {len(self)} Cards Remaining'
