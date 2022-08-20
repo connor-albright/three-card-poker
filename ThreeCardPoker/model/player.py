2f652f067af154dd02cd19b1827a11c8bb03e286
connor-albright <111525314+connor-albright@users.noreply.github.com>
2022-08-20 14:28:07 -0400

    Updated doc strings of player.py

--- ThreeCardPoker/model/player.py
+++ ThreeCardPoker/model/player.py
@@ -11,17 +11,26 @@ class HandType(Enum):
     STRAIGHT_FLUSH = 5
 
     def __gt__(self, other):
+        """Greater than override."""
         return self.value > other.value
 
     def __le__(self, other):
+        """Less than or equal to override."""
         return self.value <= other.value
         
 class Player(ABC):
     """This class represents the actions of the user or dealer (abstract)"""
     def __init__(self):
+        """Constructs the Player abstract class."""
         self.hand = tuple()
         
     def set_hand(self, cards: tuple):
+        """Sets the hand of the player.
+        Args:
+            cards (tuple): Cards that the hand is set as in a tuple.
+        Raises:
+            TypeError: if cards do not get set as a tuple while this function is called.
+        """
         if isinstance(cards, tuple): 
             self.hand = cards
         else:
@@ -33,18 +42,38 @@ class Player(ABC):
         return temp
     
     def has_pair(self):
+        """Checks to see if hand has a pair.
+        Returns:
+            int: if the length of hand returns as 2, the user has a pair since
+            duplicates are disregarded.
+        """
         return len(set([card.card_value for card in self.hand])) == 2
     
     def has_flush(self):
+        """Checks hand for flush.
+        Returns:
+            int: if the length of hand returns as 1, the user has a flush since
+            duplicates are disregarded. Suit of card is evaluated.
+        """
         return len(set([card.card_suit for card in self.hand])) == 1
     
     def has_straight(self):
+        """Checks hand for a straight.
+        Returns:
+            tuple: checks values of cards and sorts them as 1, 2, 3 if in sequential order 
+            and returns a straight if conditions are met.
+        """
         sorted_hand = sorted(self.hand)
         lowest_card_val = sorted_hand[0].card_value - 1
         sorted_hand = [x.card_value - lowest_card_val for x in sorted_hand]
         return sorted_hand == (1, 2, 3)
     
     def has_three_pair(self):
+        """Checks hand for three of a kind.
+        Returns:
+            int: if the length of hand returns as 1, the user has a flush since
+            duplicates are disregarded. Value of card is evaluated.
+        """        
         return len(set([card.card_value for card in self.hand])) == 1
     
     def evaluate_hand(self):
@@ -71,7 +100,9 @@ class Player(ABC):
         
 class Dealer(Player):
     """This class represents the dealer's parameters"""
+    
     def __init__(self):
+        """Constructs the dealer class as a subclass of player."""
         super().__init__() 
     
     def reveal_hand(self):
@@ -84,6 +115,10 @@ class User(Player):
     INITIAL_MONEY = 250.0
     
     def __init__(self, money=INITIAL_MONEY):
+        """Constructs the user class as a subclass of player.
+        Args:
+            money (float): Overall balence of the player. Defaults to INITIAL_MONEY.
+        """
         super().__init__()
         self.money = money
          
